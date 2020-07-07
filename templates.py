43c77df0a25d393ad9a5097e0cf2a0d4dc406483
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-06 20:33:17 -0400

    built rendering wiring

--- templates.py
+++ templates.py
@@ -31,6 +31,12 @@ shopping_list_html = """
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
     def get(self):
