6c4764c03648efc45269c17d3c6e0dc3e2549baa
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-06 20:01:23 -0400

    started building html form

---
+++ templates.py
@@ -0,0 +1,20 @@
+import os
+
+import webapp2
+
+form_html = """
+<form>
+<h2> Add a Food</h2>
+<input type="text" name="food">
+<button>Add</button>
+"""
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+class MainPage(Handler):
+    def get(self):
+        self.write(form_html)
+
+app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
