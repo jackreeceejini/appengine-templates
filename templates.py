00fb8ca4c97d4ce9f99a520e73ceec96cdca9e60
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-06 20:20:06 -0400

    templates with raw html substituions

--- templates.py
+++ templates.py
@@ -6,8 +6,23 @@ form_html = """
 <form>
 <h2> Add a Food</h2>
 <input type="text" name="food">
-<input type="hidden" name="food" value="eggs">
+%s
 <button>Add</button>
+"""
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+item_html = "<li>%s</li>"
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
 """
 
 class Handler(webapp2.RequestHandler):
@@ -16,6 +31,20 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.write(form_html)
+        output = form_html
+        output_hidden = ""
+
+        items = self.request.get_all("food")
+        if items:
+
+            output_items = ""
+            for item in items:
+                output_hidden += hidden_html % item
+                output_items += item_html % item
+            output_shopping = shopping_list_html % output_items
+            output += output_shopping
+        
+        output = output % output_hidden
+        self.write(output)
 
 app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
